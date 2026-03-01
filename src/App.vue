497d5ac1c21ada72f09fd725a593c57d89add47d
Cabdifataax <abdifatahiid@gmail.com>
2026-03-01 14:37:43 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hello Dev
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
